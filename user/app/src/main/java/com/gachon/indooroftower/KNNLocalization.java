b4503c0f36508e37fba059023eb3d38c001b4123
GitSoyoungYang <yco8960@gmail.com>
2023-06-10 23:52:37 +0900

    knn삽입

--- user/app/src/main/java/com/gachon/indooroftower/KNNLocalization.java
+++ user/app/src/main/java/com/gachon/indooroftower/KNNLocalization.java
@@ -125,3 +125,123 @@ package com.gachon.indooroftower;
 //}
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.neighboursearch.LinearNNSearch;
+import weka.classifiers.lazy.IBk;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class ReadAPData {
+
+    public static void main(String[] args) {
+        String csvFilePath = "/path/to/AP_c.csv";  // Replace with your CSV file path
+
+        try (FileReader fileReader = new FileReader(csvFilePath);
+             CSVParser csvParser = CSVFormat.DEFAULT.withHeader().parse(fileReader)) {
+
+            List<String> AP_bssid = new ArrayList<>();
+            List<Double> AP_location_X = new ArrayList<>();
+            List<Double> AP_location_Y = new ArrayList<>();
+
+            for (CSVRecord csvRecord : csvParser) {
+                String bssid = csvRecord.get("bssid");
+                double locationX = Double.parseDouble(csvRecord.get("X"));
+                double locationY = Double.parseDouble(csvRecord.get("y"));
+
+                AP_bssid.add(bssid);
+                AP_location_X.add(locationX);
+                AP_location_Y.add(locationY);
+            }
+
+            // Print the extracted AP data
+            System.out.println("AP BSSIDs: " + AP_bssid);
+            System.out.println("AP X Coordinates: " + AP_location_X);
+            System.out.println("AP Y Coordinates: " + AP_location_Y);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class KNNLocationFinder {
+
+    public static void main(String[] args) throws Exception {
+        // Reference point data
+        Map<String, Map<String, Double>> referencePoints = new HashMap<>();
+        referencePoints.put("RP1", createAPData(1, 2, -60, -70, -80));
+        referencePoints.put("RP2", createAPData(3, 4, -65, -75, -85));
+        referencePoints.put("RP3", createAPData(5, 6, -50, -60, -70));
+        // Add more reference points as needed
+
+        // AP data for the location to be found
+        Map<String, Double> locationData = createAPData(0, 0, -62, -73, -82);
+
+        // Prepare the training data
+        List<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("AP1"));
+        attributes.add(new Attribute("AP2"));
+        attributes.add(new Attribute("AP3"));
+
+        List<String> classes = new ArrayList<>();
+        classes.add("x");
+        classes.add("y");
+        Attribute classAttribute = new Attribute("class", classes);
+        attributes.add(classAttribute);
+
+        Instances dataset = new Instances("LocationData", attributes, referencePoints.size());
+        dataset.setClass(classAttribute);
+
+        for (Map.Entry<String, Map<String, Double>> rp : referencePoints.entrySet()) {
+            Map<String, Double> apData = rp.getValue();
+            double[] values = new double[dataset.numAttributes()];
+
+            for (int i = 0; i < attributes.size() - 1; i++) {
+                values[i] = apData.get(attributes.get(i).name());
+            }
+
+            Instance instance = new DenseInstance(1.0, values);
+            instance.setDataset(dataset);
+            dataset.add(instance);
+        }
+
+        // Perform KNN regression
+        IBk knn = new IBk();
+        knn.setKNN(3);  // Number of nearest neighbors to consider
+        knn.buildClassifier(dataset);
+
+        // Predict the location using the KNN model
+        double[] testValues = new double[dataset.numAttributes() - 1];
+        for (int i = 0; i < attributes.size() - 1; i++) {
+            testValues[i] = locationData.get(attributes.get(i).name());
+        }
+
+        Instance testInstance = new DenseInstance(1.0, testValues);
+        testInstance.setDataset(dataset);
+
+        double[] predictedLocation = knn.distributionForInstance(testInstance);
+
+        System.out.println("Predicted Location: (" + predictedLocation[0] + ", " + predictedLocation[1] + ")");
+    }
+
+    private static Map<String, Double> createAPData(double x, double y, double ap1, double ap2, double ap3) {
+        Map<String, Double> apData = new HashMap<>();
+        apData.put("AP1", ap1);
+        apData.put("AP2", ap2);
+        apData
